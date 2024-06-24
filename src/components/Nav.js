892988b68f8affc036392bd3ac748eee4cbeefee
marcinoles87 <marcinoles87@gmail.com>
2024-06-24 15:48:35 +0200

    add section

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 function Nav() {
   return (
-    <div>Nav</div>
+    <div className='nav-container'>
+      <div className='nav-list'>
+        <ul>
+          <li>About Us</li>
+          <li>Tours</li>
+          <li>Price</li>
+          <li>Transfer</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
