6dd7074b21bd3de3df592fe7bd8a789e17a4ee63
marcinoles87 <marcinoles87@gmail.com>
2024-06-25 15:34:32 +0200

    add section

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import mainImg from '../img/mainlogo.jpg'
+
 function Main() {
   return (
     <div className='main-container'>
       <div className='main-items'>
+
         <div className='main-item'>
           <i class="fa-regular fa-thumbs-up new"></i>
           <h3>Teskt</h3>
@@ -24,6 +27,13 @@ function Main() {
 
        
 
+      </div>
+
+      <div className='main-about'>
+        <div className='main-img'>
+          <img src={mainImg} alt='main-img'></img>
+        </div>
+            <p>Our company specializes in organizing private and group trips from Krakow to the most interesting places in the south of Poland. We have extensive expertise in the tourism industry. We have all required licenses and certificates. Our drivers are English-speaking. We offer airport transfer and intercity transport. We invite to cooperation individual customers as well as companies, hotels, travel agencies.</p>
       </div>
     </div>
   )
