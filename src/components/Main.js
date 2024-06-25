b00c9b60eb634b9889eea01e45cdebd040bdb0b7
marcinoles87 <marcinoles87@gmail.com>
2024-06-25 14:56:03 +0200

    add section

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,30 @@ import React from 'react'
 
 function Main() {
   return (
-    <div>Main</div>
+    <div className='main-container'>
+      <div className='main-items'>
+        <div className='main-item'>
+          <i class="fa-regular fa-thumbs-up new"></i>
+          <h3>Teskt</h3>
+          <p>Licencjonowani przewodnicy po Krakowie zapraszają na zwiedzanie Krakowa. Niezwykłe historie o naszym ukochanym mieście opowiadamy w 8 językach. Szukasz przewodników, którzy potrafią połączyć pasję, entuzjazm i perfekcyjne przygotowanie? Trafiłeś na właściwych ludzi!</p>
+        </div>
+
+        <div className='main-item'>
+          <i class="fa-regular fa-thumbs-up new"></i>
+          <h3>Teskt</h3>
+          <p>Licencjonowani przewodnicy po Krakowie zapraszają na zwiedzanie Krakowa. Niezwykłe historie o naszym ukochanym mieście opowiadamy w 8 językach. Szukasz przewodników, którzy potrafią połączyć pasję, entuzjazm i perfekcyjne przygotowanie? Trafiłeś na właściwych ludzi!</p>
+        </div>
+
+        <div className='main-item'>
+          <i class="fa-regular fa-thumbs-up new"></i>
+          <h3>Teskt</h3>
+          <p>Licencjonowani przewodnicy po Krakowie zapraszają na zwiedzanie Krakowa. Niezwykłe historie o naszym ukochanym mieście opowiadamy w 8 językach. Szukasz przewodników, którzy potrafią połączyć pasję, entuzjazm i perfekcyjne przygotowanie? Trafiłeś na właściwych ludzi!</p>
+        </div>
+
+       
+
+      </div>
+    </div>
   )
 }
 
