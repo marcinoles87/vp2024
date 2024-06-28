6f321fa86849ac685c0a05f811e1ba62e6ced971
marcinoles87 <marcinoles87@gmail.com>
2024-06-28 20:29:28 +0200

    add section

--- src/App.js
+++ src/App.js
@@ -9,8 +9,14 @@ import Footer from './components/Footer';
 import Headnav from './components/Headnav';
 import Tours from './components/Tours';
 import { Route, Routes } from 'react-router-dom';
+
 import Zakopane from './pages/Zakopane';
 import Auschwitz from './pages/Auschwitz';
+import Cracow from './pages/Cracow';
+import Czestochowa from './pages/Czestochowa';
+import Energylandia from './pages/Energylandia';
+import Wieliczka from './pages/Wieliczka';
+
 
 function App() {
 
@@ -37,6 +43,10 @@ function App() {
 
         <Route path='/zakopane' element={<Zakopane></Zakopane>}></Route>
         <Route path='/auschwitz' element={<Auschwitz></Auschwitz>}></Route>
+        <Route path='/cracow' element={<Cracow></Cracow>}></Route>
+        <Route path='/wieliczka' element={<Wieliczka></Wieliczka>}></Route>
+        <Route path='/energylandia' element={<Energylandia></Energylandia>}></Route>
+        <Route path='/czestochowa' element={<Czestochowa></Czestochowa>}></Route>
         
         
     </Routes>
