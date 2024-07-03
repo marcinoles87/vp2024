33dc858a367f6f70a75b44ae44f23538e672cb23
marcinoles87 <marcinoles87@gmail.com>
2024-07-03 16:03:24 +0200

    add section

--- src/pages/Auschwitz.js
+++ src/pages/Auschwitz.js
@@ -1,8 +1,30 @@
 import React from 'react'
 
+import imgAus from '../img/aus.jpg'
+
 function Auschwitz() {
   return (
-    <div>Auschwitz</div>
+    <div className='tour-container'>
+      <div className='tour-header'>
+        <h1>Auschwitz</h1>
+      </div>
+      <div className='tour-main'>
+        <img src={imgAus} alt=''></img>
+        <div className='tour-info'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quas dolor sed dolorem accusantium quam rem voluptates. Maiores tenetur cumque necessitatibus in iusto dicta, obcaecati, consectetur beatae vel ad quia?
+          Dolores quas sequi, assumenda eius reiciendis qui saepe deleniti necessitatibus delectus facilis nobis, tenetur voluptatem ut amet aliquid quos distinctio molestiae iste mollitia ullam voluptate iure. Rem accusantium porro et!
+          Mollitia atque voluptatem nobis vitae numquam, vel pariatur a quae ullam suscipit laborum reiciendis voluptatum iste rem, dolor amet cum ex sit. Cumque ex atque, eius voluptatem maiores a pariatur?
+          Quaerat quod vel id maxime error quas dolorem quae, magnam non nostrum. Incidunt saepe dolorum ratione fugit dicta ipsum explicabo provident voluptatum repellat quos sint sed velit, officia dignissimos maxime.
+          Possimus reprehenderit voluptatibus recusandae illum mollitia cumque animi rerum explicabo nesciunt! Minima quae deleniti quam, aspernatur accusantium blanditiis est odit a aliquid eius sequi maiores veniam? Expedita earum nostrum dolorum.
+          Voluptatem voluptas dolorum eligendi modi laudantium delectus consectetur corporis. Quaerat, ducimus alias. Odit quis laborum consectetur quod, exercitationem quo ipsam qui at, quae eius architecto est esse reiciendis, quas iure?
+          Voluptates soluta consequuntur nam ab beatae, optio corporis libero possimus doloremque consequatur vero rem qui reiciendis a sit minus harum vitae, animi atque repellat laborum? Quos eius ipsa placeat libero!
+          Eveniet voluptatem totam fugit assumenda. Exercitationem, inventore ipsam. Cumque repellat dignissimos officiis vel error dolor facere quibusdam ex maiores maxime aliquid, distinctio saepe accusantium nisi placeat animi possimus atque eligendi.
+          Quisquam sunt veritatis, expedita error dicta architecto odit. Quis neque corporis ab sequi sit alias velit deserunt illum ex ipsum voluptates asperiores, blanditiis repellendus odit dolores voluptas eum eius minima!
+          Harum ipsum quisquam, illo veniam architecto deleniti sapiente obcaecati, explicabo laboriosam suscipit itaque recusandae voluptatibus cum voluptatum dolore corrupti? Natus repellat quaerat doloribus commodi possimus dolor soluta aperiam molestiae ipsam?
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
