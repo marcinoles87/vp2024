e3da67d6b2e8ca1dde8b90539205473664cae090
marcinoles87 <marcinoles87@gmail.com>
2024-07-06 13:13:05 +0200

    add section

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,6 @@ function Header() {
       <div className='header-info'>
         <h1>Today is your Day</h1>
         <h2>Welcome to our Site</h2>
-        <button>More...</button>
       </div>
     </div>
   )
