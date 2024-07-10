c4d45412b6a948e427105cacea64232e5d7cea69
marcinoles87 <marcinoles87@gmail.com>
2024-07-10 14:42:27 +0200

    add section

--- src/pages/Price.js
+++ src/pages/Price.js
@@ -2,8 +2,62 @@ import React from 'react'
 import Table from 'react-bootstrap/Table'
 
 function Price() {
+    
   return (
-    <Table striped bordered hover >
+    <div className='price-container'>
+
+        <h1> Single group trip</h1>
+    <Table striped bordered hover variant='dark'>
+      <thead>
+        <tr>
+          
+          <th>Destination</th>
+          <th>Person</th>
+          <th>1</th>
+          <th>2</th>
+          <th>3</th>
+          <th>4</th>
+          <th>5</th>
+          <th>6-7</th>
+
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+          <td>@fat</td>
+          <td>@fat</td>
+          <td>@fat</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Larry the Bird</td>
+          <td>@twitter</td>
+          <td>@twitter</td>
+          <td>@twitter</td>
+          <td>@twitter</td>
+          <td>@twitter</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
+
+    <h1> Shared group trip</h1>
+    <Table striped bordered hover variant='dark'>
       <thead>
         <tr>
           <th>#</th>
@@ -32,6 +86,39 @@ function Price() {
         </tr>
       </tbody>
     </Table>
+
+    <h1> Transfer</h1>
+    <Table striped bordered hover variant='dark'>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
+    </div>
+   
   )
 }
 
