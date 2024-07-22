08c218232c51460f78dfa807641280579645b2f7
marcinoles87 <marcinoles87@gmail.com>
2024-07-22 19:05:48 +0200

    add section

--- src/pages/Price.js
+++ src/pages/Price.js
@@ -131,27 +131,10 @@ function Price() {
     <Table striped bordered hover variant='dark' responsive='sm'>
       <thead>
         <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
+         
         </tr>
       </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
       
-      </tbody>
     </Table>
     </div>
    
--- src/pages/Ticket.js
+++ src/pages/Ticket.js
@@ -12,7 +12,16 @@ function Ticket() {
             <div className='ticket-info'>
                 <h1>MUSEUM , MOVIE , ATRACTION ?</h1>
                 <h2>ALL FOR YOU</h2>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto sed delectus tempora dignissimos necessitatibus voluptatem iusto omnis in laudantium! Aperiam corporis dignissimos possimus molestias, doloribus id pariatur inventore quisquam illo.</p>
+                Oto cztery punkty dotyczące sprzedaży biletów do muzeum, atrakcji, kina i zabaw:
+
+<p>1. **Różnorodność ofert**: Bilety do muzeów, parków rozrywki i kin często są dostępne w różnych wariantach, takich jak bilety standardowe, rodzinne czy grupowe. To pozwala klientom na wybór najbardziej odpowiedniej opcji, dostosowanej do ich potrzeb i liczby osób.</p>
+
+<p>2. **Zakupy online**: Coraz więcej instytucji kultury i rozrywki oferuje możliwość zakupu biletów online, co znacznie ułatwia proces rezerwacji. Klienci mogą uniknąć kolejek, a dzięki dostępnym zniżkom i promocjom często zaoszczędzić na zakupach.</p>
+
+<p>3. **Promocje i rabaty**: Wielu operatorów atrakcji organizuje różnorodne promocje, takie jak dni tematyczne, zniżki dla studentów, seniorów oraz oferty specjalne w określonych dniach tygodnia. Dzięki temu zwiedzanie muzeów, kino czy korzystanie z atrakcji staje się bardziej dostępne dla szerokiego grona odbiorców.</p>
+
+<p>4. **Wydarzenia specjalne**: Bilety na wydarzenia specjalne, takie jak wystawy czasowe w muzeach, premiery filmowe czy organizowane festyny, przyciągają dodatkowych gości. Przedsiębiorstwa często promują te wydarzenia poprzez kampanie marketingowe, co zwiększa zainteresowanie i uczestnictwo w danej atrakcji.</p>
+
             </div>
         </div>
 
