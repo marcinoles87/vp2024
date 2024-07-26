b3a43ee1b5c7331d8b66cb60aaa287a7eeb5b465
marcinoles87 <marcinoles87@gmail.com>
2024-07-26 10:39:23 +0200

    add section

--- src/pages/FormContact.js
+++ src/pages/FormContact.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function FormContact() {
+
+    const [name , setName] = useState('');
+    const [email , setEmail] = useState('');
+    const [number , setNumber] = useState('');
+
+    const [submit , setSubmit] = useState('');
+
+    console.log(name , email , number)
+
   return (
     <div className='form'>
 
@@ -9,18 +18,29 @@ function FormContact() {
             <h2>Send email</h2>
             <label>
                 <p>Name</p>
-                <input type='text' placeholder='name'></input>
+                <input type='text' placeholder='name' onChange={ (e) => setName(e.target.value)}></input>
             </label>
 
             <label>
                 <p>Email</p>
-                <input type='email' placeholder='email'></input>
+                <input type='email' placeholder='email' onChange={ (e) => setEmail(e.target.value)}></input>
             </label>
 
             <label>
                 <p>Number</p>
-                <input type='number' placeholder='phone'></input>
+                <input type='number' placeholder='phone' onChange={ (e) => setNumber(e.target.value)}></input>
             </label>
+
+            <label htmlFor="tours">Choose a tour:</label>
+
+                    <select name="tours" id="tours">
+                    <option value="auschwitz">Auschwitz</option>
+                    <option value="saltmine">Wieliczka</option>
+                    <option value="cracow">Cracow</option>
+                    <option value="czestochowa">Czestochowa</option>
+                    <option value="zakopane">Zakopane</option>
+                    <option value="Energylandia">Energylandia</option>
+                    </select>
 
             <label>
                 <p>Message</p>
