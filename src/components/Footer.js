7d86cbf20fd2609c8102d9947b05cca3b17d39ff
marcinoles87 <marcinoles87@gmail.com>
2024-10-30 14:33:08 +0100

    add new function

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ function Footer() {
   return (
     <div className='footer-container'>
       <h3>grzegorzhalko@gmail.com</h3>
+      <p>+48 508-392-683</p>
     </div>
   )
 }
--- src/pages/Energylandia.js
+++ src/pages/Energylandia.js
@@ -4,6 +4,7 @@ function Energylandia() {
   return (
     <div>Energylandia
       <p>Trip to Energylandia soon......</p>
+      <p>Contact Us on +48 508-392-683</p>
     </div>
   )
 }
