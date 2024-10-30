5f6ef124a8ab14d9e2854d25fb4467e296992585
marcinoles87 <marcinoles87@gmail.com>
2024-10-30 14:16:28 +0100

    add new function

--- src/components/Headnav.js
+++ src/components/Headnav.js
@@ -4,7 +4,7 @@ function Headnav() {
   return (
     <div className='head-nav-container'>
         <ul>
-            <li><i className="fa-solid fa-phone"></i> +48 454-444-234</li>
+            <li><i className="fa-solid fa-phone"></i> +48 508-392-683</li>
             <li><i className="fa-regular fa-envelope"></i> viptour@com.pl</li>
             <li><a href='https://www.facebook.com/people/Viptourcracow-CracowAuchswitzWieliczka-Salt-Mine/100041922716636/'><i className="fa-brands fa-facebook"></i></a></li>
             {/* <li><i className="fa-brands fa-instagram"></i></li> */}
