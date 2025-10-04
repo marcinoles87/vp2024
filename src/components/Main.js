e7024900d2acf6a073b643222787eeae5aab1627
marcinoles87 <marcinoles87@gmail.com>
2025-10-04 20:39:06 +0200

    add new href

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,8 +10,7 @@ function Main() {
 
   const getDate =new Date();
   const nowYear = getDate.getFullYear()
-  console.log(nowYear)
-
+  
 
   const startCounter = () => {
 
@@ -77,17 +76,17 @@ function Main() {
             <p><span style={{fontSize:'6rem' , fontWeight:'bold'}}>We are ready </span> <br></br>Our company specializes in organizing private and group trips from Krakow to the most interesting places in the south of Poland. We have extensive expertise in the tourism industry. We have all required licenses and certificates. Our drivers are English-speaking. We offer airport transfer and intercity transport. We invite to cooperation individual customers as well as companies, hotels, travel agencies.</p>
       </div>
 
-      <div className='voucher-container'>
+      <div className='voucher-container '>
         
-        <div className='voucher-text animate__backInRight'>
-          <h1>Join our</h1>
-          <h1>Community</h1>
-          <p>If you want to get a super voucher for your next trip, join our community today. Don't hesitate, your journey starts today.</p>
-          <a href="https://www.facebook.com/people/Viptourcracow-CracowAuchswitzWieliczka-Salt-Mine/100041922716636/"><button>Get Early acces</button></a>
+        <div className='voucher-text '>
+          <h1 className='animate__animated animate__backInRight'>Join our</h1>
+          <h1 className='animate__animated animate__backInRight'>Community</h1>
+          <p className='animate__animated animate__backInRight'>If you want to get a super voucher for your next trip, join our community today. Don't hesitate, your journey starts today.</p>
+          <a href="https://www.facebook.com/people/Viptourcracow-CracowAuchswitzWieliczka-Salt-Mine/100041922716636/" className='animate__animated animate__backInRight'><button>Get Early acces</button></a>
         </div>
 
         <div className='voucher-img'>
-          <img src={voucher} alt='voucher-img'></img>
+          <img src={voucher} alt='voucher-img' className='animate__animated animate__backInRight'></img>
         </div>
             
       </div>
